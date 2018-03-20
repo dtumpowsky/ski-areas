d1b9d45456dbd609d27f08f5b66cb8772a0d7d31
David Tumpowsky and Eric Wolfe <david@>
2018-03-20 11:54:04 -0700

    display buttons that change the background color

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $("#show-NA").toggle();
+
     $("#show-SA").hide();
   });
 
   $(".clickTwo").click(function() {
     $("#show-SA").toggle();
     $("#show-NA").hide();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#blue").click(function() {
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
